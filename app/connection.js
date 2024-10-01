f5f24877b3be929622f8e33df5417b731e69ab07
100fgitacc <100fsiteblog@gmail.com>
2024-10-01 11:01:38 +0000

    new DB connection

--- app/connection.js
+++ app/connection.js
@@ -1,6 +1,7 @@
 
 import { Pool } from 'pg';
-const connectionString = "postgres://default:RAQX5hP6aTuk@ep-withered-base-a4foo3ho.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
+// const connectionString = "postgres://default:RAQX5hP6aTuk@ep-withered-base-a4foo3ho.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require";
+const connectionString = "postgresql://admin:r9hZmRdc5qAdry7RUmL@85.209.154.162:5432/webinar";
 
 const pool = new Pool({
   connectionString,
