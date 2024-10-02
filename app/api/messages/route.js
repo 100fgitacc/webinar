a5960c9e142f4c97ecf5071c13547259792dd8cd
100fgitacc <100fsiteblog@gmail.com>
2024-10-02 13:59:10 +0000

    change online data format 2

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -283,6 +283,7 @@ async function updatePinnedStatus(messageId, pinned) {
   }
 }
 async function updateAndBroadcastPinnedStatus(messageId, pinned) {
+  
   try {
     const messagePayload = {
       messageId,
